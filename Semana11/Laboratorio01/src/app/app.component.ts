1de28e747e89e2adcfe284f95f33a13bcbd8c92b
A. Santiago Nunez <abraham.nunez@tecsup.edu.pe>
2024-10-24 21:48:56 -0500

    Semana10 Laboratorio 11, parte 3/4 de los ejercicios planteados finalizado

--- Semana11/Laboratorio01/src/app/app.component.ts
+++ Semana11/Laboratorio01/src/app/app.component.ts
@@ -10,12 +10,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Apli01';
-  valor = "Nuñez Garcia A. Santiago";
-  tipo = "password";
-  img = "assets/angular.png";
-  enlace = "https://youtube.com";
-  clase01 = "azul";
-  clase02 = "rojo";
-  alttxt = "Imagen increible";
+  onClick(){
+    alert('Utilizando Eventos, en este caso el evento Onclick');
+  }
 }
