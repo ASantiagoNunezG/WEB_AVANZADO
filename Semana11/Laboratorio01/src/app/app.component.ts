cbc58974057fa0afb63e4e299a91bea515e0de7a
A. Santiago Nunez <abraham.nunez@tecsup.edu.pe>
2024-10-24 22:04:14 -0500

    Semana10 Laboratorio 11, ejercicios propuestos finalizado 4/4

--- Semana11/Laboratorio01/src/app/app.component.ts
+++ Semana11/Laboratorio01/src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   //standalone: true,
-  //imports:[RouterOutlet, CommonModule],
+  //imports:[RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  onClick(){
-    alert('Utilizando Eventos, en este caso el evento Onclick');
-  }
+  nombre: string = '';
 }
