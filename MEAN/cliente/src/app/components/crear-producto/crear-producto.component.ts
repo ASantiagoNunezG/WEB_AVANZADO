f8cf34ca7d81f36bf0bf8a8bf18959c9c14a524e
A. Santiago Nunez <abraham.nunez@tecsup.edu.pe>
2024-11-20 23:35:00 -0500

    Laboratorio 13, desarrollo de aplicaciones con MEAN, terminado

---
+++ MEAN/cliente/src/app/components/crear-producto/crear-producto.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Producto } from 'src/app/models/producto';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-crear-producto',
+  templateUrl: './crear-producto.component.html',
+  styleUrls: ['./crear-producto.component.css']
+})
+export class CrearProductoComponent {
+  productoForm: FormGroup;
+  titulo = 'Crear producto';
+
+  constructor(private fb: FormBuilder, private router:Router){
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  agregarProducto(){
+    const PRODUCTO: Producto = {
+      nombre: this.productoForm.get('producto')?.value,
+      categoria: this.productoForm.get('categoria')?.value,
+      ubicacion: this.productoForm.get('ubicacion')?.value,
+      precio: this.productoForm.get('precio')?.value,
+    }
+    console.log(PRODUCTO)
+    this.router.navigate(['/']);
+  }
+}
