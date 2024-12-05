3c18fd9d8223a519738cb7b72a0f342fb3ccbbbe
A. Santiago Nunez <abraham.nunez@tecsup.edu.pe>
2024-12-05 09:10:18 -0500

    proyecto funcional

--- Semana16/FrontendNunez/angular-app/src/app/services/auth.service.ts
+++ Semana16/FrontendNunez/angular-app/src/app/services/auth.service.ts
@@ -5,36 +5,36 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8080/auth'; // Cambiar a tu URL de API
+    private apiUrl = 'http://localhost:8080/auth'; // Cambiar a tu URL de API
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  // Método para registrar al usuario
-  register(user: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/register`, user);
-  }
+    // Método para registrar al usuario
+    register(user: any): Observable<any> {
+        return this.http.post(`${this.apiUrl}/register`, user);
+    }
 
-  // Método para iniciar sesión
-  login(credentials: any): Observable<any> {
-    // Indicamos que la respuesta es de tipo 'text' para evitar el error de JSON
-    return this.http.post(`${this.apiUrl}/login`, credentials, { responseType: 'text' });
-  }
+    // Método para iniciar sesión
+    login(credentials: any): Observable<any> {
+        // Indicamos que la respuesta es de tipo 'text' para evitar el error de JSON
+        return this.http.post(`${this.apiUrl}/login`, credentials, { responseType: 'text' });
+    }
 
-  // Método para obtener el token almacenado
-  getToken(): string | null {
-    return localStorage.getItem('token');
-  }
+    // Método para obtener el token almacenado
+    getToken(): string | null {
+        return localStorage.getItem('token');
+    }
 
-  // Método para guardar el token
-  saveToken(token: string): void {
-    localStorage.setItem('token', token);
-  }
+    // Método para guardar el token
+    saveToken(token: string): void {
+        localStorage.setItem('token', token);
+    }
 
-  // Método para eliminar el token
-  logout(): void {
-    localStorage.removeItem('token');
-  }
+    // Método para eliminar el token
+    logout(): void {
+        localStorage.removeItem('token');
+    }
 }
